71ab5e9980277c01f31c51b8b88f765fc14b068d
Konrad Czajka <konrad.czajka@xstream.net>
2016-10-26 18:02:38 +0200

    Add .bash_profile to installation script

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Symlinks
 
+ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
