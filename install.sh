51bc28e0ad341bf782c6e25ae48d84d8e18d02bb
Konrad Czajka <konrad.czajka@xstream.net>
2016-10-26 18:09:27 +0200

    Add clean-up to installation script

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Clean-up
+rm ~/.bash_profile
+rm ~/.gitconfig
+rm ~/.gitignore_global
+
 # Symlinks
 
 ln -sfv "$DOTFILES_DIR/.bash_profile" ~
