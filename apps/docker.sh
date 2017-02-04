b814838898f391c90320abe5182ce231242025bb
Konrad Czajka <konrad.czajka@xstream.net>
2017-02-04 15:08:18 +0100

    Add docker installer

---
+++ apps/docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Installing Docker and Docker Compose"
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv \
+               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install docker-engine
+sudo gpasswd -a nte docker
+sudo service docker restart
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
