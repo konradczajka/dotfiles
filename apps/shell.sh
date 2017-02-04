c8e6c1762e22b62d08ef15fb5b8db32fe9371495
Konrad Czajka <konrad.czajka@xstream.net>
2017-02-04 15:09:15 +0100

    Add zsh installer

---
+++ apps/shell.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo Installing Zzh
+#sudo apt-get install -y zsh
+
+echo Changing default shell
+#chsh -s $(which zsh)
+
+echo Installing Oh My Zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo Installing Powerline fonts
+rm -r /tmp/fonts
+mkdir /tmp/fonts
+wget https://github.com/powerline/fonts/tarball/master -O /tmp/fonts/archive.tar.gz 
+tar -xvf /tmp/fonts/archive.tar.gz -C /tmp/fonts --strip-components=1 --exclude='{.gitignore}'
+sudo /tmp/fonts/install.sh
+rm -r /tmp/fonts
