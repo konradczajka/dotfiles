33cbbb2256685abcfc1a8cb36d0b6d3ae94c201f
Konrad Czajka <konrad.czajka@xstream.net>
2019-03-20 08:43:14 +0100

    cleanup

--- apps/git-kraken.hs
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm /tmp/git-kraken.deb
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb -O /tmp/git-kraken.deb
-sudo dpkg -i /tmp/git-kraken.deb
-rm /tmp/git-kraken.deb
--- apps/rest-client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-echo "Installing Insomnia REST client"
-rm /tmp/insomnia.deb
-wget https://builds.insomnia.rest/downloads/ubuntu/latest -O /tmp/insomnia.deb
-sudo dpkg -i /tmp/insomnia.deb
-rm /tmp/insomnia.deb
