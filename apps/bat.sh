907e12760f08c9cd9fb0433ff8308df5054fc536
Konrad Czajka <konrad.czajka@gmail.com>
2021-07-03 19:37:13 +0200

    bat upgrade

--- apps/bat.sh
+++ apps/bat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/sharkdp/bat/releases/download/v0.8.0/bat_0.8.0_amd64.deb -O /tmp/bat.deb
+wget https://github.com/sharkdp/bat/releases/download/v0.18.1/bat_0.18.1_amd64.deb -O /tmp/bat.deb
 sudo dpkg -i /tmp/bat.deb
 rm /tmp/bat.deb
